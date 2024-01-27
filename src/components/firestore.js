69884965a9b23f828abc1fba39572d1dc9e2231b
lazytime <0205miss@gmail.com>
2024-01-27 21:51:03 +0800

    UPDATE PIOS

--- src/components/firestore.js
+++ src/components/firestore.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAnXisFrVmqgfxBLlLlwKIFLw4yoXm6NPc",
-  authDomain: "easygoods-823a0.firebaseapp.com",
-  projectId: "easygoods-823a0",
-  storageBucket: "easygoods-823a0.appspot.com",
-  messagingSenderId: "877767861878",
-  appId: "1:877767861878:web:5278c713cd7afa30c1e157",
-  measurementId: "G-9FKF2J6JWY"
+  apiKey: process.env.FIRESTORE_APIKEY,
+  authDomain: process.env.FIRESTORE_AUTHDOMAIN,
+  projectId: process.env.FIRESTORE_PROJECTID,
+  storageBucket: process.env.FIRESTORE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIRESTORE_SENDERID,
+  appId: process.env.FIRESTORE_APPID,
+  measurementId: process.env.FIRESTORE_MEASUREMENTID
 };
 
 // Initialize Firebase
