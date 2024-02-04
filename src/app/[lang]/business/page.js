567063bfb9f804ec373e5c40c6ea1ee588f55b0e
lazytime <0205miss@gmail.com>
2024-02-05 02:18:04 +0800

    update

--- src/app/[lang]/business/page.js
+++ src/app/[lang]/business/page.js
@@ -1,8 +1,67 @@
-import "server-only";
+'use client'
+import { Button, Dropdown, DropdownMenu, DropdownSection, DropdownTrigger,DropdownItem } from "@nextui-org/react";
+import { useContext, useEffect, useState } from "react";
+import { OwnerContext } from "./business";
+import LoadingPage from "@/components/loading";
+
 
 export default function BusinessPage() {
-
-  return <div className="w-screen h-screen">
-
+  const snap = useContext(OwnerContext)
+  const [currentshop,setcurrentshop] = useState(null)
+  const [shoplist,setshoplist] = useState([])
+  useEffect(()=>{
+    if(snap==null) return
+    if(snap.empty){
+      setcurrentshop('')
+    }else{
+      let count = 0
+      if(shoplist.length!=0){
+        setshoplist([])
+      }
+      snap.forEach(doc => {
+        let data = doc.data()
+        if(count==0){
+          setcurrentshop(data.name)
+          count++
+        }
+        setshoplist([...shoplist,data])
+      });
+    }
+  },[snap])
+  if(snap==null) return <LoadingPage/>
+  return <div className="w-full h-full">
+    <div className="flex w-full px-5 py-3">
+    <Dropdown>
+    <DropdownTrigger>
+      <Button variant="bordered" className="w-full">
+        {currentshop==''?"You don't have business yet":currentshop}
+      </Button>
+    </DropdownTrigger>
+    <DropdownMenu variant="faded" aria-label="" className="w-80">
+      {currentshop=='' ? null : <DropdownSection showDivider>
+        
+      {
+        shoplist.map(doc=>{
+          return <DropdownItem
+                key={doc.name}
+              >
+                {doc.name}
+              </DropdownItem>
+        })
+      }
+      </DropdownSection>}
+      <DropdownSection>
+      <DropdownItem
+            key="new"
+            shortcut="+"
+            description="Create a new shop"
+          >
+            Add Bussiness
+          </DropdownItem>
+      </DropdownSection>
+    </DropdownMenu>
+    </Dropdown>
+    </div>
+      
     </div>;
 }
--- src/app/[lang]/layout.js
+++ src/app/[lang]/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { i18n } from "../../../locale-config";
 import {Analytics} from '@vercel/analytics/react'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import NextUi from "@/components/nextui";
 
 export async function generateStaticParams() {
   return i18n.locales.map((locale) => ({ lang: locale }));
@@ -30,7 +31,9 @@ export default function RootLayout({ children,params }) {
   return (
     <html lang={params.lang}>
       <body className={hind_siliguri.className}>
+        <NextUi>
         {children}
+        </NextUi>        
         <Analytics />
         <SpeedInsights/>
       </body>
