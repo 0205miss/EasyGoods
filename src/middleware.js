b9bb13bd7c2ba957bf86c30a8da8c851ce51fe1a
lazytime <0205miss@gmail.com>
2024-02-15 22:47:38 +0800

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -59,5 +59,5 @@ export function middleware(request) {
 
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|zh-TW|zh-CN).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|zh-TW|zh-CN|validation-key.txt).*)"],
 };
