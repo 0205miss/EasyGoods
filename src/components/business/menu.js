000980a764a7a0084af4c39edb1d7f036dd943be
lazytime <0205miss@gmail.com>
2024-02-23 23:29:17 +0800

    add app order switch on menu page

--- src/components/business/menu.js
+++ src/components/business/menu.js
@@ -4,9 +4,39 @@ import { useState } from "react";
 
 export default function MenuBusiness({ data }) {
   const [piaccept, setpiaccept] = useState(data.payment);
+  const [orderaccept, setorderaccept] = useState(data.apporder);
   return (
     <div className="w-full h-full px-4 py-2">
       <Switch
+        isSelected={orderaccept}
+        onValueChange={setorderaccept}
+        classNames={{
+          base: cn(
+            "inline-flex flex-row-reverse w-full max-w-md bg-content1 hover:bg-content2 items-center",
+            "justify-between cursor-pointer rounded-lg gap-2 p-4 border-2 border-transparent",
+            "data-[selected=true]:border-primary"
+          ),
+          wrapper: "p-0 h-4 overflow-visible",
+          thumb: cn(
+            "w-6 h-6 border-2 shadow-lg",
+            "group-data-[hover=true]:border-primary",
+            //selected
+            "group-data-[selected=true]:ml-6",
+            // pressed
+            "group-data-[pressed=true]:w-7",
+            "group-data-[selected]:group-data-[pressed]:ml-4"
+          ),
+        }}
+      >
+        <div className="flex flex-col gap-1">
+          <p className="text-medium">EasyGoods Order</p>
+          <p className="text-tiny text-default-400">
+            Customer can make order on this app
+          </p>
+        </div>
+      </Switch>
+      <Divider className="my-2" />
+      {orderaccept && <><Switch
         isSelected={piaccept}
         onValueChange={setpiaccept}
         classNames={{
@@ -34,7 +64,8 @@ export default function MenuBusiness({ data }) {
           </p>
         </div>
       </Switch>
-      <Divider className="my-2" />
+      <Divider className="my-2" /></>}
+      
 
       <div className="w-full flex h-10 stroke-accent-500 justify-center">
         <Button
