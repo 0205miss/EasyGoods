a4624c888e0db1507d33da4bcacb66e3c7c9be5e
lazytime <0205miss@gmail.com>
2024-02-25 00:14:23 +0800

    add product editor

--- src/components/business/updateproduct.js
+++ src/components/business/updateproduct.js
@@ -16,19 +16,38 @@ import {
 import { Upload } from "@/res/icon/upload";
 import { useEffect, useState } from "react";
 import { Garbage } from "@/res/icon/garbage";
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { addDoc, collection, doc } from "firebase/firestore";
+import { ref, uploadBytesResumable, getDownloadURL,deleteObject, } from "firebase/storage";
+import { deleteDoc, doc, setDoc } from "firebase/firestore";
 import { db, storage } from "../firestore";
+import { v4 as uuidv4 } from 'uuid';
 
-export default function MenuModal({ isOpen, onOpenChange, data,onClose,setmenu }) {
+export default function UpdateProductModal({
+  isOpen,
+  shopId,
+  onOpenChange,
+  data,
+  onClose,
+  setproduct,
+  index,
+}) {
   const [submit, setsubmit] = useState(false);
-  const [imageurl, setimageurl] = useState(0);
-  const [upload, setupload] = useState(0);
-  const [cost, setcost] = useState('1.0000000');
-  const [description, setdescription] = useState("");
-  const [name, setname] = useState('');
-  const [time, settime] = useState('0');
-    const [checkvalid,setvalid] = useState(false)
+  const [imageurl, setimageurl] = useState(data.pricture);
+  const [upload, setupload] = useState(1);
+  const [cost, setcost] = useState(data.cost.toString());
+  const [description, setdescription] = useState(data.description);
+  const [name, setname] = useState(data.name);
+  const [time, settime] = useState(data.time.toString());
+  const [checkvalid, setvalid] = useState(false);
+
+useEffect(()=>{
+    setimageurl(data.pricture)
+    setcost(data.cost.toString())
+    setdescription(data.description)
+    setname(data.name)
+    settime(data.time.toString())
+},[])
+
+
   const fileupload = async (target) => {
     const image = target.files ? target.files[0] : null;
     if (!image) {
@@ -38,33 +57,62 @@ export default function MenuModal({ isOpen, onOpenChange, data,onClose,setmenu }
     setimageurl(URL.createObjectURL(image));
   };
 
-  useEffect(()=>{
-    if(cost!=''&&name!=''&&time!=''&&upload!=0){
-        setvalid(true)
-    }else{
-        setvalid(false)
+  useEffect(() => {
+    if (
+      imageurl == data.pricture &&
+      cost == data.cost.toString() &&
+      description == data.description &&
+      name == data.name &&
+      time == data.time.toString()
+    ) {
+      setvalid(false);
+    } else {
+      if (cost != "" && name != "" && time != "" && upload != 0) {
+        setvalid(true);
+      } else {
+        setvalid(false);
+      }
     }
-  },[cost,time,description,name,upload])
+  }, [cost, time, description, name, upload,description]);
 
   const onsubmit = async () => {
-    setsubmit(true)
-    const product = {
+    setsubmit(true);
+    const newproduct = {
       cost: parseFloat(cost),
       description: description,
       name: name,
       time: parseInt(time),
     };
-    const url = await updateImage(data.id,upload)
-    const res = await createproduct(data,url,product)
-    onClose()
-    setmenu(old => [...old,res])
-    setimageurl(0)
-    setupload(0)
-    setcost('1.0000000')
-    setdescription('')
-    setname('')
-    settime('0')
-    setsubmit(false)
+    
+    let url 
+    if(imageurl == data.pricture){
+        url = imageurl
+    }else{
+        await deleteImage(data.pricture);
+        url = await updateImage(shopId, upload);
+    }
+    const res = await createproduct(shopId, url, newproduct,data.id);
+    setvalid(false);
+    setsubmit(false);
+    onClose();
+    setproduct(old =>{
+        let a = [...old]
+        a[index] = res
+        return a
+    });
+  };
+
+  const ondelete = async () => {
+    setsubmit(true);
+    await deleteproduct(shopId, data.id);
+    await deleteImage(data.pricture);
+    setsubmit(false);
+    onClose();
+    setproduct(old =>{
+        let a = [...old]
+        a[index] = 0
+        return a
+    });    
   };
 
   return (
@@ -78,7 +126,7 @@ export default function MenuModal({ isOpen, onOpenChange, data,onClose,setmenu }
       backdrop="blur"
     >
       <ModalContent>
-        <ModalHeader>Product Creation</ModalHeader>
+        <ModalHeader>Product Edit</ModalHeader>
         <ModalBody>
           <div
             className=" w-full h-40 flex justify-center items-center bg-white p-3"
@@ -108,9 +156,9 @@ export default function MenuModal({ isOpen, onOpenChange, data,onClose,setmenu }
                   <div className="absolute right-2 top-2 !z-30">
                     <Button
                       onClick={() => {
-                        setimageurl(0)
-                        setupload(0)
-                    }}
+                        setimageurl(0);
+                        setupload(0);
+                      }}
                       isIconOnly
                       className="text-white fill-white p-1"
                       color="danger"
@@ -180,8 +228,11 @@ export default function MenuModal({ isOpen, onOpenChange, data,onClose,setmenu }
           />
         </ModalBody>
         <ModalFooter className="!justify-center">
+          <Button color="danger" onClick={ondelete}>
+            Delete
+          </Button>
           <Button color="warning" onClick={onsubmit} isDisabled={!checkvalid}>
-            Create
+            Update
           </Button>
         </ModalFooter>
         {submit && (
@@ -210,17 +261,18 @@ async function updateImage(Id, image) {
 }
 
 async function uploadImage(Id, image) {
-  const filePath = `${Id}/menu/${image.name}`;
+    const uid = uuidv4()
+  const filePath = `${Id}/menu/${uid}/${image.name}`;
   const newImageRef = ref(storage, filePath);
   await uploadBytesResumable(newImageRef, image);
 
   return await getDownloadURL(newImageRef);
 }
 
-async function createproduct(data, url, product) {
-  const shopRef = doc(db, "shop", data.id);
-  const menuRef = collection(shopRef, "menu");
-  const newdoc = await addDoc(menuRef, {
+async function createproduct(Id, url, product,menuId) {
+    console.log(Id+'///'+menuId)
+  const menuRef = doc(db, "shop", Id,'menu',menuId);
+  const id = await setDoc(menuRef, {
     cost: product.cost,
     description: product.description,
     name: product.name,
@@ -228,11 +280,27 @@ async function createproduct(data, url, product) {
     time: product.time,
   });
   return {
-    id:newdoc.id,
+    id: menuId,
     cost: product.cost,
     description: product.description,
     name: product.name,
     pricture: url,
     time: product.time,
-  }
+  };
+}
+
+async function deleteImage(url) {
+  const httpsRef = ref(storage, url);
+  deleteObject(httpsRef)
+    .then(() => {
+      return 1;
+    })
+    .catch((error) => {
+      return 0;
+    });
+}
+
+async function deleteproduct(Id, menuId) {
+  await deleteDoc(doc(db, "shop", Id, "menu", menuId));
+  return true;
 }
