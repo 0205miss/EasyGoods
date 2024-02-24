81d489503aced1a92156b66ac961b9b496fbb547
lazytime <0205miss@gmail.com>
2024-02-24 12:35:57 +0800

    change redirect by user clicking

--- src/app/[lang]/getuserlocation/page.js
+++ src/app/[lang]/getuserlocation/page.js
@@ -1,26 +1,29 @@
 "use client";
 
-import { useEffect } from "react";
-import { Spinner } from "@nextui-org/react";
+import { useEffect, useState } from "react";
+import { Button, Spinner } from "@nextui-org/react";
+import Link from 'next/link'
 
 export default function GetUserPage({ params }) {
+  const [location,setlocation] = useState(null)
   useEffect(() => {
     if ("geolocation" in navigator) {
       // Retrieve latitude & longitude coordinates from `navigator.geolocation` Web API
 
       navigator.geolocation.getCurrentPosition(({ coords }) => {
         const { latitude, longitude } = coords;
-        location.assign(
-          process.env.NEXT_PUBLIC_APP_DOMAIN +
-            params.lang +
-            `/map/${latitude}/${longitude}`
-        );
+        setlocation({
+          latitude,longitude
+        })
       });
     }
   }, []);
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <Spinner color="warning" size="lg" />
+      {location==null && <Spinner color="warning" size="lg" />}
+      {location!=null && <Link href={`pi://testnet.easygoods.app/map/${location.latitude}/${location.longitude}`}>
+        <Button color="secondary">Go Back Pi Browser</Button>
+        </Link>}
     </div>
   );
 }
