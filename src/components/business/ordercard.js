df7ad9270ee03013649aeb11b19aca8c4d4e0d13
lazytime <0205miss@gmail.com>
2024-02-26 16:31:41 +0800

    stamp card coming soon

--- src/components/business/ordercard.js
+++ src/components/business/ordercard.js
@@ -1,17 +1,14 @@
-import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/react";
 import { Tabs, Tab } from "@nextui-org/react";
 
-export default function OrderCard({ order,transcript }) {
+export default function OrderCard({ order, transcript }) {
   return (
-    <div className="w-full h-full px-4 py-2">
-      <Card className="w-full h-full bg-primary">
-        <CardHeader className="flex justify-center">
-          <Tabs color="secondary">
-            <Tab key="progress" title={transcript["Progress"]}></Tab>
-            <Tab key="history" title={transcript["History"]}></Tab>
-          </Tabs>
-        </CardHeader>
-      </Card>
+    <div className="w-full h-full px-4 py-2 flex flex-col">
+      <div className="flex justify-center">
+        <Tabs color="secondary" classNames={{tabList:['bg-white']}}>
+          <Tab key="progress" title={transcript["Progress"]}></Tab>
+          <Tab key="history" title={transcript["History"]}></Tab>
+        </Tabs>
+      </div>
     </div>
   );
 }
