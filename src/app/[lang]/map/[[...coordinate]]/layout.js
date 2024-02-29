89b7a6911c75a36392dda6efd3dcd40c33f46ae8
lazytime <0205miss@gmail.com>
2024-02-29 21:50:21 +0800

    change route structure

--- src/app/[lang]/map/[[...coordinate]]/layout.js
+++ src/app/[lang]/map/[[...coordinate]]/layout.js
@@ -1,10 +1,19 @@
 import Link from "next/link";
+import PiUser from "../pi";
 export default function UserLayout({ children, params }) {
+  let subpath=''
+  if(params.coordinate!=undefined){
+    subpath=`/${params.coordinate[0]}/${params.coordinate[1]}`
+  }
   return (
     <div className="w-screen h-screen">
-      <div className="w-full h-full pb-16 z-0">{children}</div>
+      <div className="w-full h-full pb-16 z-0">
+        <PiUser>
+        {children}
+        </PiUser>
+      </div>
       <div className="fixed w-full bg-[#c3cfe2] shadow-xl bottom-0 h-16 grid grid-flow-col gap-px pt-[1px]">
-        <Link href={`/${params.lang}/order`}>
+        <Link href={`/${params.lang}/order${subpath}`}>
           <div className="flex justify-center stroke-ui-primary bg-ui-background items-center h-full">
             <svg
               width="35px"
@@ -44,33 +53,6 @@ export default function UserLayout({ children, params }) {
             </svg>
           </div>
         </Link>
-        <Link href={`/${params.lang}/map`}>
-          <div className="flex bg-ui-background stroke-ui-primary justify-center items-center h-full">
-            <svg
-              width="35px"
-              height="35px"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-              <g
-                id="SVGRepo_tracerCarrier"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></g>
-              <g id="SVGRepo_iconCarrier">
-                {" "}
-                <path
-                  d="M12 6H12.01M9 20L3 17V4L5 5M9 20L15 17M9 20V14M15 17L21 20V7L19 6M15 17V14M15 6.2C15 7.96731 13.5 9.4 12 11C10.5 9.4 9 7.96731 9 6.2C9 4.43269 10.3431 3 12 3C13.6569 3 15 4.43269 15 6.2Z"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></path>{" "}
-              </g>
-            </svg>
-          </div>
-        </Link>
         <div className="flex bg-ui-background stroke-ui-accent justify-center items-center h-full">
           <svg
             width="35px"
@@ -87,20 +69,47 @@ export default function UserLayout({ children, params }) {
             ></g>
             <g id="SVGRepo_iconCarrier">
               {" "}
-              <circle cx="12" cy="9" r="3" strokeWidth="1.5"></circle>{" "}
               <path
-                d="M17.9691 20C17.81 17.1085 16.9247 15 11.9999 15C7.07521 15 6.18991 17.1085 6.03076 20"
+                d="M12 6H12.01M9 20L3 17V4L5 5M9 20L15 17M9 20V14M15 17L21 20V7L19 6M15 17V14M15 6.2C15 7.96731 13.5 9.4 12 11C10.5 9.4 9 7.96731 9 6.2C9 4.43269 10.3431 3 12 3C13.6569 3 15 4.43269 15 6.2Z"
                 strokeWidth="1.5"
                 strokeLinecap="round"
-              ></path>{" "}
-              <path
-                d="M7 3.33782C8.47087 2.48697 10.1786 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 10.1786 2.48697 8.47087 3.33782 7"
-                strokeWidth="1.5"
-                strokeLinecap="round"
+                strokeLinejoin="round"
               ></path>{" "}
             </g>
           </svg>
         </div>
+        <Link href={`/${params.lang}/menu${subpath}`}>
+          <div className="flex bg-ui-background stroke-ui-primary justify-center items-center h-full">
+            <svg
+              width="35px"
+              height="35px"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                {" "}
+                <circle cx="12" cy="9" r="3" strokeWidth="1.5"></circle>{" "}
+                <path
+                  d="M17.9691 20C17.81 17.1085 16.9247 15 11.9999 15C7.07521 15 6.18991 17.1085 6.03076 20"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                ></path>{" "}
+                <path
+                  d="M7 3.33782C8.47087 2.48697 10.1786 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 10.1786 2.48697 8.47087 3.33782 7"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                ></path>{" "}
+              </g>
+            </svg>
+          </div>
+        </Link>
       </div>
     </div>
   );
