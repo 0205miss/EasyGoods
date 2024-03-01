a026d85d6082f709ae4022bd1d046ce93130c7f9
lazytime <0205miss@gmail.com>
2024-03-01 16:49:16 +0800

    redirect automatic in pi browser

--- src/app/[lang]/getuserlocation/page.js
+++ src/app/[lang]/getuserlocation/page.js
@@ -2,33 +2,58 @@
 
 import { useEffect, useState } from "react";
 import { Button, Spinner } from "@nextui-org/react";
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function GetUserPage({ params }) {
-  const [location,setlocation] = useState(null)
+  const [location, setlocation] = useState(null);
   useEffect(() => {
     if ("geolocation" in navigator) {
       // Retrieve latitude & longitude coordinates from `navigator.geolocation` Web API
 
-      navigator.geolocation.getCurrentPosition(({ coords }) => {
-        const { latitude, longitude } = coords;
-        setlocation({
-          latitude,longitude
-        })
-      },()=>{
-        alert('EasyGoods suggest user to access the location permission for user experience')
-        setlocation({
-          latitude:0,longitude:0
-        })
-      });
+      navigator.geolocation.getCurrentPosition(
+        ({ coords }) => {
+          const { latitude, longitude } = coords;
+          setlocation({
+            latitude,
+            longitude,
+          });
+          if (
+            window.location.ancestorOrigins[0] ==
+              "https://sandbox.minepi.com" ||
+            window.location.ancestorOrigins[0] ==
+              "https://app-cdn.minepi.com" ||
+            window.location.ancestorOrigins[0] ==
+              "https://easygoods5604.pinet.com"
+          ) {
+            location.assign(
+              "pi://testnet.easygoods.app/" +
+                params.lang +
+                `/map/${latitude}/${longitude}`
+            );
+          }
+        },
+        () => {
+          alert(
+            "EasyGoods suggest user to access the location permission for user experience"
+          );
+          setlocation({
+            latitude: 0,
+            longitude: 0,
+          });
+        }
+      );
     }
   }, []);
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      {location==null && <Spinner color="warning" size="lg" />}
-      {location!=null && <Link href={`pi://testnet.easygoods.app/map/${location.latitude}/${location.longitude}`}>
-        <Button color="secondary">Go Back Pi Browser</Button>
-        </Link>}
+      {location == null && <Spinner color="warning" size="lg" />}
+      {location != null && (
+        <Link
+          href={`pi://testnet.easygoods.app/${params.lang}/map/${location.latitude}/${location.longitude}`}
+        >
+          <Button color="secondary">Go Back Pi Browser</Button>
+        </Link>
+      )}
     </div>
   );
 }
