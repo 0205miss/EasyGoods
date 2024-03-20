fc026a68146638d045e0a64b4d30e4eaaedf3295
lazytime <0205miss@gmail.com>
2024-03-21 07:05:32 +0800

    remove speed insight

--- src/app/[lang]/layout.js
+++ src/app/[lang]/layout.js
@@ -1,8 +1,6 @@
 import { Hind_Siliguri } from "next/font/google";
 import "./globals.css";
 import { i18n } from "../../../locale-config";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import NextUi from "@/components/nextui";
 
 export async function generateStaticParams() {
@@ -43,8 +41,6 @@ export default function RootLayout({ children, params }) {
       </head>
       <body className={hind_siliguri.className}>
         <NextUi>{children}</NextUi>
-        <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   );
